8625cc50d6dea42eba9e4295251624dce1c8aa7c
Kookaipersan <kookaipersan@gmail.com>
2025-06-23 13:40:14 +0200

    correction index.js

--- backend/models/index.js
+++ backend/models/index.js
@@ -1,12 +1,21 @@
 const { Sequelize, DataTypes } = require("sequelize");
-const dbConfig = require("../config/db.config");
 
-const sequelize = new Sequelize(dbConfig.DB, dbConfig.USER, dbConfig.PASSWORD, {
-  host: dbConfig.HOST,
-  dialect: dbConfig.DIALECT, // ex: 'mysql'
-  port: dbConfig.PORT,
-  pool: dbConfig.pool,
-});
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
+  {
+    host: process.env.DB_HOST,
+    dialect: 'postgres',
+    port: process.env.DB_PORT || 5432,
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false
+      }
+    }
+  }
+);
 
 const db = {};
 db.Sequelize = Sequelize;

d5ebc4a1496216f3c88644b7ca17f60aac7f15ef
Kookaipersan <kookaipersan@gmail.com>
2025-06-23 11:59:39 +0200

    mise a jour base pour postgreSQL Render

--- backend/config/db.config.js
+++ backend/config/db.config.js
@@ -1,14 +1,12 @@
 module.exports = {
-  HOST: "localhost",
-  USER: "root",
-  PASSWORD: "", // ← Remplace par ton vrai mot de passe
-  DB: "trouve_ton_artisan", // ← ou le nom exact de ta base de données
-  DIALECT: "mysql",
-  PORT: 3306,
-  pool: {
-    max: 5,
-    min: 0,
-    acquire: 30000,
-    idle: 10000,
+  development: {
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    port: 5432, 
+    dialect: 'postgres',
+    logging: false,
   },
 };
+
